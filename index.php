28c9758689ef530ec6a3a60a5168164f524c6a54
AbidHasanKhondaker <149704252+AbidHasanKhondaker@users.noreply.github.com>
2024-08-26 21:49:28 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Learn Hackathon</title>
+</head>
+
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="navbar">
+        <div class="org">
+          <a href="#main">
+            <h1><span>H</span>ack <span>T</span>rail</h1>
+          </a>
+        </div>
+        <div class="navlist">
+          <div class="dropdown">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#main" data-after="Home">Home</a></li>
+            <li><a href="#resources" data-after="resource">Resources</a></li>
+            <li><a href="#courses" data-after="courses">Courses</a></li>
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="/abid's_file/signin.php" data-after="About">Sign up/ Log in</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- main Section  -->
+  <section id="main">
+    <div class="main container">
+      <div>
+        <h1>Hello, <span></span></h1>
+        <h1>We are HackTrail <span></span></h1>
+        <h1>Want to learn hackathon? <span></span></h1>
+        <a href="#courses" type="button" class="cta">Get started</a>
+      </div>
+    </div>
+  </section>
+  <!-- End main Section  -->
+
+  <!-- resource Section -->
+  <section id="resources">
+    <div class="resources container">
+      <div class="resource-top">
+        <h1 class="section-title"><span>r</span>esou<span>r</span>ces</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas
+          magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias
+          voluptatum explicabo!</p>
+      </div>
+      <div class="resource-bottom">
+        <div class="resource-item">
+          <div class="icon"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8srWss8hKnPEBxM2gpQ_3nz8rjmk9ZmQdnsAjq_IV8as447vME4s7CjoTNSnTunDmtII&usqp=CAU" /></div>
+          <h2>Web Design</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="resource-item">
+          <div class="icon"><img src="https://d2ms8rpfqc4h24.cloudfront.net/mobile_application_development_ad3f711b7d.jpg" /></div>
+          <h2>App Dev</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="resource-item">
+          <div class="icon"><img src="https://www.freelancinggig.com/blog/wp-content/uploads/2018/11/Start-a-Blockchain-Development-Company.jpg" /></div>
+          <h2>Block Chain</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+        <div class="resource-item">
+          <div class="icon"><img src="https://www.einfochips.com/wp-content/uploads/2018/10/how-to-develop-machine-learning-applications-for-business-featured.jpg" /></div>
+          <h2>Machine Learning</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed
+            architecto placeat beatae tenetur officia quod</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End resource Section -->
+
+  <!-- courses Section -->
+  <section id="courses">
+    <div class="courses container">
+      <div class="courses-header">
+        <h1 class="section-title"><span>C</span>ourses</h1>
+      </div>
+      <div class="all-courses">
+        <div class="course-item">
+          <div class="course-info">
+            <h1>Web dev</h1>
+            <h2>Html Css Javascript</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="course-img">
+            <img src="./img/img1.png" alt="img">
+          </div>
+        </div>
+        <div class="course-item">
+          <div class="course-info">
+            <h1>learn Django</h1>
+            <h2>Create web with Python</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="course-img">
+            <img src="./img/img2.png" alt="img">
+          </div>
+        </div>
+        <div class="course-item">
+          <div class="course-info">
+            <h1>App Devlopement</h1>
+            <h2>A market with the biggest possibility</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="course-img">
+            <img src="./img/img3.png" alt="img">
+          </div>
+        </div>
+        <div class="course-item">
+          <div class="course-info">
+            <h1>Blockchain</h1>
+            <h2>A distributed database</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="course-img">
+            <img src="./img/img4.png" alt="img">
+          </div>
+        </div>
+        <div class="course-item">
+          <div class="course-info">
+            <h1>Machine learning</h1>
+            <h2>AI is the future</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde
+              rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt
+              harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
+          </div>
+          <div class="course-img">
+            <img src="./img/img5.png" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End courses Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="./img/buet.png" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">About <span>us</span></h1>
+        <h2>A beginer friendly platform</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, velit alias eius non illum beatae atque
+          repellat ratione qui veritatis repudiandae adipisci maiores. At inventore necessitatibus deserunt
+          exercitationem cumque earum omnis ipsum rem accusantium quis, quas quia, accusamus provident suscipit magni!
+          Expedita sint ad dolore, commodi labore nihil velit earum ducimus nulla quae nostrum fugit aut, deserunt
+          reprehenderit libero enim!</p>
+        <!-- <a href="#" class="cta">Download Resume</a> -->
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>01736943322</h2>
+            <h2>01710024924</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>hacktrail@gmail.com</h2>
+            <h2>hacktrail_customer_care@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>ECE Building, BUET, Dhaka-1205, Bangladesh</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="org">
+        <h1>Hack Trail</h1>
+      </div>
+      <h2>Find your inner talent</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="https://www.facebook.com/profile.php?id=100034743932433"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="https://www.instagram.com/tasnim_jarin_momo_?igsh=MW9qZTF4OHowem51eg=="><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/?size=100&id=13963&format=png&color=000000" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/behance.png" /></a>
+        </div>
+      </div>
+      
+    </div>
+  </section>
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+</body>
+
+</html>
