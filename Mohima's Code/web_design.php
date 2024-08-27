98926cd357bd2452a2966361a1114f12c7752148
Adibah Mahima <adibahmahima@gmail.com>
2024-08-27 11:04:47 +0600

    Added my folder

---
+++ Mohima's Code/web_design.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Learn Hackathon</title>
+</head>
+
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="navbar">
+        <div class="org">
+          <a href="#main">
+            <h1><span>H</span>ack <span>T</span>rail</h1>
+          </a>
+        </div>
+        <div class="navlist">
+          <div class="dropdown">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#main" data-after="Home">Home</a></li>
+            <li><a href="#resources" data-after="resource">Resources</a></li>
+            <li><a href="#courses" data-after="courses">Courses</a></li>
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="/abid's_file/signin.php" data-after="About">Sign up/ Log in</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="org">
+        <h1>Hack Trail</h1>
+      </div>
+      <h2>Find your inner talent</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="https://www.facebook.com/profile.php?id=100034743932433"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="https://www.instagram.com/tasnim_jarin_momo_?igsh=MW9qZTF4OHowem51eg=="><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/?size=100&id=13963&format=png&color=000000" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/behance.png" /></a>
+        </div>
+      </div>
+      
+    </div>
+  </section>
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+</body>
+
+</html>
