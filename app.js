3a0cb8c9aa84a23a4328b2942950f19e8bbceae4
AbidHasanKhondaker <149704252+AbidHasanKhondaker@users.noreply.github.com>
2024-08-28 18:13:52 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const dropdown = document.querySelector('.header .navbar .navlist .dropdown');
+const mobile_menu = document.querySelector('.header .navbar .navlist ul');
+const menu_item = document.querySelectorAll('.header .navbar .navlist ul li a');
+const header = document.querySelector('.header.container');
+
+dropdown.addEventListener('click', () => {
+	dropdown.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		dropdown.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
